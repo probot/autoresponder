137910b201f1d690031d3b35fadd001a9de726b8
hiimbex <hiimbex@github.com>
2017-07-03 20:01:12 -0700

    updating stuff and new try catch

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 module.exports = robot => {
   robot.on('issues.opened', async context => {
-    const options = context.repo({path: '.github/ISSUE_REPLY_TEMPLATE.md'});
-    const res = await context.github.repos.getContent(options);
-    const template = new Buffer(res.data.content, 'base64').toString();
-
+    let template;
+    try {
+      const options = context.repo({path: '.github/ISSUE_REPLY_TEMPLATE.md'});
+      const res = await context.github.repos.getContent(options);
+      template = new Buffer(res.data.content, 'base64').toString();
+    } catch (err) {
+      template = 'We couldn\'t find an `ISSUE_REPLY_TEMPLATE` in your `.github` folder!';
+    }
+    robot.log(template);
     return context.github.issues.createComment(context.issue({body: template}));
   });
 };
