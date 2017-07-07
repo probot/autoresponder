8b806d8743ca4c2c38b6f1421dfe8f43d1148340
hiimbex <hiimbex@github.com>
2017-07-07 12:58:31 -0700

    fixed linter issues

--- index.js
+++ index.js
@@ -6,8 +6,11 @@ module.exports = robot => {
       const res = await context.github.repos.getContent(options);
       template = new Buffer(res.data.content, 'base64').toString();
     } catch (err) {
-        if (err.code === 404) template = 'We couldn\'t find an `ISSUE_REPLY_TEMPLATE` in your `.github` folder!';
-        else throw err;
+      if (err.code === 404) {
+        template = 'We couldn\'t find an `ISSUE_REPLY_TEMPLATE` in your `.github` folder!';
+      } else {
+        throw err;
+      }
     }
 
     return context.github.issues.createComment(context.issue({body: template}));
