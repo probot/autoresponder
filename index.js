c529faf854abbeb91bc74db795bbcec7d1d9eea3
Bex Warner <bexmwarner@gmail.com>
2017-09-05 21:32:26 +0200

    Use standard for linting

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 module.exports = robot => {
   robot.on('issues.opened', async context => {
-    const options = context.repo({path: '.github/ISSUE_REPLY_TEMPLATE.md'});
-    const res = await context.github.repos.getContent(options);
-    const template = Buffer.from(res.data.content, 'base64').toString();
+    const options = context.repo({path: '.github/ISSUE_REPLY_TEMPLATE.md'})
+    const res = await context.github.repos.getContent(options)
+    const template = Buffer.from(res.data.content, 'base64').toString()
 
-    return context.github.issues.createComment(context.issue({body: template}));
-  });
-};
+    return context.github.issues.createComment(context.issue({body: template}))
+  })
+}
