62b8d9fb3d1deccf98f685e58a84f00ce29dec85
Aastha Bist <abist119@gmail.com>
2020-07-01 10:26:36 +0530

    Update getContent() method to getContents()

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 module.exports = robot => {
   robot.on('issues.opened', async context => {
     const options = context.repo({path: '.github/ISSUE_REPLY_TEMPLATE.md'})
-    const res = await context.github.repos.getContent(options)
+    const res = await context.github.repos.getContents(options)
     const template = Buffer.from(res.data.content, 'base64').toString()
 
     return context.github.issues.createComment(context.issue({body: template}))
